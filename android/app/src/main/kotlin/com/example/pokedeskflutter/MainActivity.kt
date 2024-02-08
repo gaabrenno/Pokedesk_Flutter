a1d5f9b51a37ed362766183f3392504137128e3e
Gabriel Brenno <97069315+gaabrenno@users.noreply.github.com>
2024-02-07 21:59:47 -0300

    Revert "fixed: connection to firebase"

--- android/app/src/main/kotlin/com/example/pokedeskflutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.pokedeskflutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,29 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:pokedesk/app/app_widget.dart';
+import 'package:pokedesk/checagem_page.dart';
 import 'package:pokedesk/firebase_options.dart';
+import 'package:flutter/material.dart';
+import 'home_page.dart';
 
-main() async {
+Future<void> main() async {
+  runApp(const MyApp());
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform
-  );
-  
-  runApp(
-    const AppWidget()
-  );
+    options: DefaultFirebaseOptions.currentPlatform,);
+        runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pokedesk',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const ChecagemPage(),
+    );
+  }
 }
